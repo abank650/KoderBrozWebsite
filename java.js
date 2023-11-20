064fc489ab48ad24a09ddab57fe5b4347debf075
banko650 <abb115@georgetown.edu>
2023-11-20 21:26:33 +0100

    added javascript

---
+++ java.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        burger.classList.toggle('toggle');
+    });
+});
+
+
