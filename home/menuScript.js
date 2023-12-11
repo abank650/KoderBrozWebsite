f992dfc36cfde465a2cfc3bd388e99dc8ec1ffe4
murphybotko <134979662+murphybotko@users.noreply.github.com>
2023-12-11 14:01:55 +0100

    Added menu items

--- home/menuScript.js
+++ home/menuScript.js
@@ -13,4 +13,27 @@ document.addEventListener('DOMContentLoaded', () => {
             burger.classList.remove('toggle');
         }
     });
+
+    // Tab functionality
+    const tabButtons = document.querySelectorAll('.tab-button');
+    const tabContents = document.querySelectorAll('.tab-content');
+
+    tabButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const targetTab = button.getAttribute('onclick').match(/'([^']+)'/)[1];
+
+            // Hide all tab contents and remove active class from all buttons
+            tabContents.forEach(content => content.style.display = 'none');
+            tabButtons.forEach(btn => btn.classList.remove('active'));
+
+            // Show the clicked tab content and set button as active
+            document.getElementById(targetTab).style.display = 'block';
+            button.classList.add('active');
+        });
+    });
+
+    // Trigger click on the first tab to open it by default
+    if (tabButtons.length > 0) {
+        tabButtons[0].click();
+    }
 });
